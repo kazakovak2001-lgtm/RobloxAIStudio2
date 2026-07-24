8977013b59ae4fb8cd0e42d0786c6a5c98eda586
kazakovak2001-lgtm <kazakov.ak.2001@gmail.com>
2026-07-24 18:41:23 +0200

    ci(core): allow style commits and technical acronyms

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,22 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "refactor",
+        "perf",
+        "docs",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "style",
+      ],
+    ],
+    "subject-case": [0],
+  },
 };
