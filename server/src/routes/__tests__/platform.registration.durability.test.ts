89eb06f83cedbd129f9a946405df4b557b0bb965
kazakovak2001-lgtm <kazakov.ak.2001@gmail.com>
2026-07-29 13:51:35 +0200

    test(registration): intercept canonical account transaction

--- server/src/routes/__tests__/platform.registration.durability.test.ts
+++ server/src/routes/__tests__/platform.registration.durability.test.ts
@@ -8,6 +8,7 @@ import {
   DurableStorageError,
   InMemoryStorageProvider,
   type DurableMutation,
+  type DurableMutationResult,
 } from "../../platform/storage/StorageProvider";
 import { UserRepository } from "../../platform/users/UserRepository";
 import { createPlatformRouter } from "../platform";
@@ -16,13 +17,16 @@ import type { ProjectAccessControl } from "../projects";
 class RejectableRegistrationStorage extends InMemoryStorageProvider {
   rejectBatch = false;
 
-  override async mutateDurably(
+  override async applyDurableBatch(
     mutations: readonly DurableMutation[],
-  ): Promise<void> {
+  ): Promise<readonly DurableMutationResult[]> {
     if (this.rejectBatch) {
-      throw new DurableStorageError("injected registration rejection", "batch");
+      throw new DurableStorageError(
+        "injected registration rejection",
+        "transaction",
+      );
     }
-    await super.applyDurableBatch(mutations);
+    return super.applyDurableBatch(mutations);
   }
 }
 
