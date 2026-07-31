2dc8c6d23d293ebb1271270ddcc232117289c24d
kazakovak2001-lgtm <kazakov.ak.2001@gmail.com>
2026-07-31 12:43:49 +0200

    fix(data-207): clean postgres operational exports

--- server/src/platform/storage/postgres/index.ts
+++ server/src/platform/storage/postgres/index.ts
@@ -6,12 +6,7 @@ import {
   type StorageFailureCategory,
   type StorageOperationalStatus,
 } from "../StorageProvider";
-import {
-  PostgresStorageProvider as BasePostgresStorageProvider,
-  DEFAULT_POSTGRES_CONFIG,
-  type PostgresConfig,
-  type PostgresStorageDependencies,
-} from "./PostgresStorageProvider";
+import { PostgresStorageProvider as BasePostgresStorageProvider } from "./PostgresStorageProvider";
 
 class StorageOperationalState {
   private lastFailure?: {
